bc80c5cbdb1daff655e7f11ff612815e287f0a18
Pulkit Tyagi <pulkittyagi@192.168.0.196>
2022-09-20 08:45:49 +0530

    pulkit fixed the code

--- prime.js
+++ prime.js
@@ -1,10 +1,20 @@
-function prime(num){
-	for (let i=1;i<=num;i++){
-		if(num%i==0){
-			return true;
+function prime(number){
+	let factors=0;
+	for (let i=1;i<=number;i++){
+		if(number%i==0){
+			factors++;
 		}
+	}
+	if(factors==2){
+		return true;
 	}
 	return false;
 }
 
-prime(13);
+let x=prime(13);
+
+if(x==true){
+	console.log("prime");
+} else {
+	console.log("not prime");
+}
